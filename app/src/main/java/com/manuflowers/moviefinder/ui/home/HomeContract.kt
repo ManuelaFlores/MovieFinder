cfde77d6fb2c50fd05b703fd2224951e2437ed59
Manuela Flores <fmanuela499@gmail.com>
2018-12-05 01:03:56 -0500

    implement interface

--- app/src/main/java/com/manuflowers/moviefinder/ui/home/HomeContract.kt
+++ app/src/main/java/com/manuflowers/moviefinder/ui/home/HomeContract.kt
@@ -1,4 +1,33 @@
 package com.manuflowers.moviefinder.ui.home
 
+import com.manuflowers.moviefinder.ui.model.Result
+
 interface HomeContract {
+    /**
+     * BasePresenter layer.
+     */
+
+    interface  View {
+        fun showLoading()
+
+        fun hideLoading()
+
+        fun onSuccesfull(list:List<Result>)
+
+        fun showErrorService(errorMessage: String)
+
+        fun showFailureMessage(failureMessage: String?)
+    }
+
+    interface Presenter {
+        fun requestDataMovie(query: String?)
+    }
+
+    interface Callback {
+        fun onErrorService(code:String)
+
+        fun onFailureService(failureMessage: String?)
+
+        fun onSuccessful(list:List<Result>)
+    }
 }
